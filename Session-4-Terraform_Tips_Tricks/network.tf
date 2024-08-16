76638141ee9e2f45b2541627e7deee361b0f11c4
Krishanu Chakraborty <krishanuc1001@users.noreply.github.com>
2024-08-16 19:11:49 +0530

    Session 4 goal 1 | Remove commented resources

--- Session-4-Terraform_Tips_Tricks/network.tf
+++ Session-4-Terraform_Tips_Tricks/network.tf
@@ -9,30 +9,6 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-# Public Subnets
-# resource "aws_subnet" "subnet_public_1" {
-#   vpc_id                  = aws_vpc.vpc.id
-#   cidr_block              = var.subnet1_cidr
-#   map_public_ip_on_launch = "true"
-#   availability_zone       = format("%sa", var.region)
-#
-#   tags = {
-#     Name = format("%s-public-subnet-1", var.prefix)
-#   }
-# }
-#
-#
-# resource "aws_subnet" "subnet_public_2" {
-#   vpc_id                  = aws_vpc.vpc.id
-#   cidr_block              = var.subnet2_cidr
-#   map_public_ip_on_launch = "true"
-#   availability_zone       = format("%sb", var.region)
-#
-#   tags = {
-#     Name = format("%s-public-subnet-2", var.prefix)
-#   }
-# }
-
 resource "aws_subnet" "subnet_public" {
   count                   = var.number_of_public_subnets
   vpc_id                  = aws_vpc.vpc.id
@@ -45,30 +21,6 @@ resource "aws_subnet" "subnet_public" {
   }
 }
 
-# Private Subnets
-# resource "aws_subnet" "subnet_private_1" {
-#   vpc_id                  = aws_vpc.vpc.id
-#   cidr_block              = var.subnet3_cidr
-#   map_public_ip_on_launch = "false"
-#   availability_zone       = format("%sa", var.region)
-#
-#   tags = {
-#     Name = format("%s-private-subnet-1", var.prefix)
-#   }
-# }
-#
-#
-# resource "aws_subnet" "subnet_private_2" {
-#   vpc_id                  = aws_vpc.vpc.id
-#   cidr_block              = var.subnet4_cidr
-#   map_public_ip_on_launch = "false"
-#   availability_zone       = format("%sb", var.region)
-#
-#   tags = {
-#     Name = format("%s-private-subnet-2", var.prefix)
-#   }
-# }
-
 resource "aws_subnet" "subnet_private" {
   for_each                = {for i in range(var.number_of_private_subnets) : i => i}
   vpc_id                  = aws_vpc.vpc.id
@@ -80,29 +32,6 @@ resource "aws_subnet" "subnet_private" {
     Name = format("%s-private-subnet-%d", var.prefix, each.key + 1)
   }
 }
-
-# Secure subnets
-# resource "aws_subnet" "subnet_secure_1" {
-#   vpc_id                  = aws_vpc.vpc.id
-#   cidr_block              = var.subnet5_cidr
-#   map_public_ip_on_launch = "false"
-#   availability_zone       = format("%sa", var.region)
-#
-#   tags = {
-#     Name = format("%s-secure-subnet-1", var.prefix)
-#   }
-# }
-#
-# resource "aws_subnet" "subnet_secure_2" {
-#   vpc_id                  = aws_vpc.vpc.id
-#   cidr_block              = var.subnet6_cidr
-#   map_public_ip_on_launch = "false"
-#   availability_zone       = format("%sb", var.region)
-#
-#   tags = {
-#     Name = format("%s-secure-subnet-2", var.prefix)
-#   }
-# }
 
 resource "aws_subnet" "subnet_secure" {
   for_each                = {for i in range(var.number_of_secure_subnets) : i => i}
@@ -169,16 +98,6 @@ resource "aws_route_table" "private_routetable" {
 }
 
 # Route Table Association for Public Subnets
-# resource "aws_route_table_association" "public_subnet_1" {
-#   subnet_id      = aws_subnet.subnet_public_1.id
-#   route_table_id = aws_route_table.public_routetable.id
-# }
-
-# resource "aws_route_table_association" "public_subnet_2" {
-#   subnet_id      = aws_subnet.subnet_public_2.id
-#   route_table_id = aws_route_table.public_routetable.id
-# }
-
 resource "aws_route_table_association" "public_subnet" {
   count          = 2
   subnet_id      = aws_subnet.subnet_public[count.index].id
@@ -186,16 +105,6 @@ resource "aws_route_table_association" "public_subnet" {
 }
 
 # Route Table Association for Private Subnets
-# resource "aws_route_table_association" "private_subnet_1" {
-#   subnet_id      = aws_subnet.subnet_private_1.id
-#   route_table_id = aws_route_table.private_routetable.id
-# }
-#
-# resource "aws_route_table_association" "private_subnet_2" {
-#   subnet_id      = aws_subnet.subnet_private_2.id
-#   route_table_id = aws_route_table.private_routetable.id
-# }
-
 resource "aws_route_table_association" "private_subnet" {
   for_each       = aws_subnet.subnet_private
   subnet_id      = each.value.id
