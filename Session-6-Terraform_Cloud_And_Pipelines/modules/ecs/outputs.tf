998b943c686be1589d632f7a47a194f709a438e8
Krishanu Chakraborty <krishanuc1001@users.noreply.github.com>
2024-08-25 14:50:29 +0530

    Session 6 Goal 1 | Delete Session 6 files, keep only Session 1...5 in tf-sessions branch

--- Session-6-Terraform_Cloud_And_Pipelines/modules/ecs/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ecr_url" {
-  description = "The Elastic Container Registry (ECR) URL."
-  value       = aws_ecr_repository.api.repository_url
-}
-
-output "ecs_security_group_id" {
-  description = "ECS Security group Id"
-  value       = aws_security_group.ecs.id
-}
